980b9e4cc8b38950740735f138c3930fcff43b2b
Lukas Polak <lpolak@dioscouri.com>
2016-03-16 15:04:43 +0100

    deleted temporary python files

--- tools/dialogs/partshelper.py
+++
@@ -1,7 +0,0 @@
-
-# this is helper class for manipulation with dialog parts
-class PartsHelper:
-
-	# this method should extract all noun phrases from provided text
-	def ExtractNounPhrases(self, text):
-		return []
